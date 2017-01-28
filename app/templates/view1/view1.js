3673d6362cd3be88e12fedffd98a0e4c7de8b5d0
Omar Lopez <omarlopez1808@gmail.com>
2017-01-27 22:25:48 -0400

    final commit

--- app/templates/view1/view1.js
+++ app/templates/view1/view1.js
@@ -4,6 +4,12 @@ angular.module('myApp.view1', ['ngRoute'])
 
 
 .controller('View1Ctrl', ['$http','$scope', function($http, $scope) {
+$scope.selected = "";
+$scope.cart = [];
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
 
 setTimeout(function(){
  $('.collapsible-header').dropdown({
@@ -23,7 +29,124 @@ $http.get('json/data.json')
 .success(function(data){
     $scope.categorias = data.categories;
     $scope.productos = data.products;
-    console.log($scope.productos);
+    
+    for (var i = 0; i < $scope.productos.length; i++) {
+          var int = data.products[i].price.replace(".","");
+          $scope.productos[i].number = parseInt(int);
+      }
+    $scope.copy = $scope.productos;
     })
+
+$scope.hasChanged = function() {
+  console.log($scope.selected);
+    $scope.products = [];
+    if($scope.selected.length == 1){
+  
+      for (var i = 0; i < $scope.copy.length; i++) {
+        
+          for (var j = 0; j < $scope.copy[i].categories.length; j++){
+            
+            if($scope.copy[i].categories[j] == $scope.selected){
+              $scope.products.push($scope.copy[i]);
+              console.log($scope.products);
+            }
+
+          }
+
+      }
+
+      $scope.productos =[];
+      $scope.productos = $scope.products;
+      $scope.products = [];
+    }
+
+    if($scope.selected == "available"){
+      for (var i = 0; i < $scope.copy.length; i++){
+        if($scope.copy[i].available){
+          $scope.products.push($scope.copy[i]);
+        }
+      }
+      $scope.productos =[];
+      $scope.productos = $scope.products;
+      $scope.products = [];
+    }
+
+    if($scope.selected == "not"){
+      for (var i = 0; i < $scope.copy.length; i++){
+        if(!$scope.copy[i].available){
+          $scope.products.push($scope.copy[i]);
+        }
+      }
+      $scope.productos =[];
+      $scope.productos = $scope.products;
+      $scope.products = [];
+    }
+
+    if($scope.selected == "best_seller"){
+      for (var i = 0; i < $scope.copy.length; i++){
+        if($scope.copy[i].best_seller){
+          $scope.products.push($scope.copy[i]);
+        }
+    }
+      $scope.productos =[];
+      $scope.productos = $scope.products;
+      $scope.products = [];
+    }
+
+    if($scope.selected == "bigger"){
+      for (var i = 0; i < $scope.copy.length; i++){
+        if($scope.copy[i].number > 30000){
+          $scope.products.push($scope.copy[i]);
+        }
+    }
+      $scope.productos =[];
+      $scope.productos = $scope.products;
+      $scope.products = [];
+    }
+
+    if($scope.selected == "lower"){
+      for (var i = 0; i < $scope.copy.length; i++){
+        if($scope.copy[i].number <= 10000){
+          $scope.products.push($scope.copy[i]);
+        }
+    }
+      $scope.productos =[];
+      $scope.productos = $scope.products;
+      $scope.products = [];
+    }   
+
+    if($scope.selected == "all"){
+      $scope.productos =[];
+      $scope.productos = $scope.copy;
+      $scope.products = [];
+    }   
+
+}
+
+$scope.openModalConfirmation = function(item){
+  $scope.item = item;
+  $('#modal2').modal('open');
+}
+
+$scope.closeModalConfirmation = function(){
+  $scope.cart.push($scope.item)
+  console.log($scope.cart);
+  $('#modal2').modal('close');
+}
+
+$scope.openRemoveItem = function(item){
+  $('#modal4').modal('open');
+  $scope.remove = item;
+}
+
+$scope.closeRemoveItem = function(){
+  $('#modal4').modal('close');
+
+  for(var i = 0; i < $scope.cart.length; i++){
+    if($scope.cart[i].id == $scope.remove.id){
+      $scope.cart.splice(i,1);
+    }
+  }
+}
 
 }]);
